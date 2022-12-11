a94d41d93b483852c32b88fdc7697a059c7ecc61
abraham aditya <abraham.sudjatmoko@binus.edu>
2022-12-12 00:18:34 +0700

    Update color

--- lib/helper/color.dart
+++ lib/helper/color.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
+Color whiteDefault = const Color.fromARGB(255, 250, 250, 250);
 Color black = const Color.fromARGB(255, 0, 0, 0);
 Color grey = const Color.fromARGB(255, 125, 125, 125);
 Color lightGrey = const Color.fromARGB(255, 197, 201, 216);
 Color white = const Color.fromARGB(255, 255, 255, 255);
 Color yellow = const Color.fromARGB(255, 253, 252, 220);
+Color yellowGradient = const Color.fromARGB(255, 253, 252, 220);
 Color orange = const Color.fromARGB(255, 255, 184, 0);
+Color orangeGradient = const Color.fromARGB(255, 255, 204, 72);
 Color paleOrange = const Color.fromARGB(255, 255, 220, 128);
 Color red = const Color.fromARGB(255, 237, 10, 63);
 Color pink = const Color.fromARGB(255, 255, 99, 99);
 Color green = const Color.fromARGB(255, 13, 204, 13);
+Color blue = const Color.fromARGB(255, 0, 175, 185);
 Color mint = const Color.fromARGB(255, 185, 229, 227);
 Color teal = const Color.fromARGB(255, 0, 175, 185);
+Color greyblue = const Color.fromARGB(255, 235, 235, 242);
