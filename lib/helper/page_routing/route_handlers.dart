11bb7c5eeae999d0a0bba92d1c304943b3b8f9e7
abraham aditya <abraham.sudjatmoko@binus.edu>
2022-12-12 00:18:15 +0700

    Update page routing

--- lib/helper/page_routing/route_handlers.dart
+++ lib/helper/page_routing/route_handlers.dart
@@ -1,7 +1,17 @@
-import 'package:caridiskon/page/detail_penawaran_page.dart';
-import 'package:caridiskon/page/home_page.dart';
+import 'package:caridiskon/page/contact_us_page.dart';
+import 'package:caridiskon/page/detail_semua_penawaran_page.dart';
+import 'package:caridiskon/page/detail_top_20_penawaran_page.dart';
+import 'package:caridiskon/page/detail_tukar_poin_page.dart';
 import 'package:caridiskon/page/login_page.dart';
+import 'package:caridiskon/page/home_page.dart';
+import 'package:caridiskon/page/kategori_page.dart';
+import 'package:caridiskon/page/register_page.dart';
+import 'package:caridiskon/page/search_page.dart';
+import 'package:caridiskon/page/semua_penawaran_page.dart';
+import 'package:caridiskon/page/semua_toko_page.dart';
+import 'package:caridiskon/page/toko_page.dart';
 import 'package:caridiskon/page/top_20_penawaran_page.dart';
+import 'package:caridiskon/page/tukar_poin_page.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +25,42 @@ var top20PenawaranHandler = Handler(
   return Top20PenawaranPage();
 });
 
+var semuaPenawaranHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return SemuaPenawaranPage();
+});
+
+var tukarPoinHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return TukarPoinPage();
+});
+
+var semuaTokoHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return SemuaTokoPage();
+});
+
+var detailTokoHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["id"]![0];
+    return DetailTokoPage(id: data);
+  },
+);
+
+var searchHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["value"]![0];
+    return SearchPage(value: data);
+  },
+);
+
+var kategoriHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["name"]![0];
+    return KategoriPage(name: data);
+  },
+);
+
 // Approach 1
 // var detailPenawaranHandler = Handler(
 //   handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
@@ -26,14 +72,52 @@ var top20PenawaranHandler = Handler(
 // );
 
 // Approach 2
-var detailPenawaranHandler = Handler(
+var detailTop20PenawaranHandler = Handler(
   handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
     String data = params["id"]![0];
-    return DetailPenawaranPage(id: data);
+    return DetailTop20PenawaranPage(id: data);
+  },
+);
+
+var detailAllPenawaranHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["id"]![0];
+    return DetailAllPenawaranPage(id: data);
+  },
+);
+
+var detailTukarPoinHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["id"]![0];
+    return DetailAllPenawaranPage(id: data);
+  },
+);
+
+var detailTrendingHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["id"]![0];
+    return DetailAllPenawaranPage(id: data);
+  },
+);
+
+var detailEkslusifHandler = Handler(
+  handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    String data = params["id"]![0];
+    return DetailAllPenawaranPage(id: data);
   },
 );
 
 var loginHandler = Handler(
     handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
-  return const LoginPage();
+  return LoginPage();
+});
+
+var registerHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return RegisterPage();
+});
+
+var contactUsHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return ContactUsPage();
 });
--- lib/helper/page_routing/routes.dart
+++ lib/helper/page_routing/routes.dart
@@ -5,8 +5,20 @@ import 'route_handlers.dart';
 class Routes {
   static String home = "/";
   static String top20Penawaran = "/top20penawaran";
-  static String detailPenawaran = "/top20penawaran/:id";
+  static String semuaPenawaran = "/semuapenawaran";
+  static String tukarPoin = "/tukarpoin";
+  static String detailTukarPoin = "/tukarpoin/:id";
+  static String detailTop20Penawaran = "/top20penawaran/:id";
+  static String detailAllPenawaran = "/semuapenawaran/:id";
+  static String detailTrending = "/trending/:id";
+  static String detailEkslusif = "/ekslusif/:id";
+  static String kategori = "/kategori/:name";
   static String login = "/login";
+  static String register = "/register";
+  static String semuaToko = "/toko";
+  static String contactUs = "/contactus";
+  static String detailToko = "/toko/:id";
+  static String search = "/pencarian/:value";
 
   static void configureRoutes(FluroRouter router) {
     router.notFoundHandler = Handler(
@@ -23,8 +35,73 @@ class Routes {
         transitionType: TransitionType.inFromLeft,
         transitionDuration: const Duration(milliseconds: 100));
 
-    router.define(detailPenawaran,
-        handler: detailPenawaranHandler,
+    router.define(detailTop20Penawaran,
+        handler: detailTop20PenawaranHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(detailAllPenawaran,
+        handler: detailAllPenawaranHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(kategori,
+        handler: kategoriHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(semuaPenawaran,
+        handler: semuaPenawaranHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(tukarPoin,
+        handler: tukarPoinHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(detailTukarPoin,
+        handler: detailTukarPoinHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(semuaToko,
+        handler: semuaTokoHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(detailToko,
+        handler: detailTokoHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(detailTrending,
+        handler: detailTrendingHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(detailEkslusif,
+        handler: detailEkslusifHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(login,
+        handler: loginHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(register,
+        handler: registerHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(search,
+        handler: searchHandler,
+        transitionType: TransitionType.inFromLeft,
+        transitionDuration: const Duration(milliseconds: 100));
+
+    router.define(contactUs,
+        handler: contactUsHandler,
         transitionType: TransitionType.inFromLeft,
         transitionDuration: const Duration(milliseconds: 100));
 
